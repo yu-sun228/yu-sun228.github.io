84f92f77aacce0a40137b49d8b9859e4795d579d
yu-sun228 <dimlightsat4am@gmail.com>
2026-01-02 20:49:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const panel = document.getElementById("panel");
-const buttons = document.querySelectorAll(".side-nav button");
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const targetId = button.dataset.target;
-    const targetSection = document.getElementById(targetId);
-
-    panel.scrollTo({
-      top: targetSection.offsetTop,
-      behavior: "smooth"
-    });
-  });
-});
